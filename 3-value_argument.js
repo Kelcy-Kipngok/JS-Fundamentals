006b3a3651426ee929dbc2f080679f61c4fce53d
Kelcy-Kipngok <kelcykipngok@gmail.com>
2025-07-12 21:45:07 +0300

    Task3 corrections

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -3,7 +3,7 @@ const args = process.argv;
 if (args[2] === undefined) {
   console.log("No argument");
 } else if (args[3] === undefined) {
-  console.log(`one argument: "${args[2]}"`);
+  console.log(`HBTN`);
 } else if (args[4] === undefined) {
-  console.log(`2 arguments: "${args[2]} ${args[3]}"`);
+  console.log(`HBTN cool`);
 }
