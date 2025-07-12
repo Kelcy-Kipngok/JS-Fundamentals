079ef0efe18d8b74b306e4c259f29d482923052b
Kelcy <kelcykipngok@gmail.com>
2025-07-12 22:17:30 +0300

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -3,7 +3,7 @@ const args = process.argv;
 if (args[2] === undefined) {
   console.log("No argument");
 } else if (args[3] === undefined) {
-  console.log('Argument found');
+  console.log('HBTN');
 } else {
   console.log(`2 arguments: "${args[2]} ${args[3]}"`);
 }
