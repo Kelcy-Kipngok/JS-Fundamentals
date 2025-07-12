90ec10823c182b978e40588c8339d670d35907bb
Kelcy-Kipngok <kelcykipngok@gmail.com>
2025-07-12 21:42:51 +0300

    Task 3 correction

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -2,4 +2,8 @@ const args = process.argv;
 
 if (args[2] === undefined) {
   console.log("No argument");
+} else if (args[3] === undefined) {
+  console.log(`one argument: "${args[2]}"`);
+} else if (args[4] === undefined) {
+  console.log(`2 arguments: "${args[2]} ${args[3]}"`);
 }
